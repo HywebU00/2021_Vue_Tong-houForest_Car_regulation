798c1f5d60e6034baf805409da48790ae40cb1b3
Your Namemargo-lee <margo.lee@hyweb.com.tw>
2021-06-01 14:11:54 +0800

    修改tab 元件

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import Header from"@/components/Header.vue";
 import Footer from"@/components/Footer.vue";
 import TabContent from"@/components/TabContent.vue";
 import $ from 'jquery';
+import { onMounted } from '@vue/runtime-core';
 
 export default {
   components:{
     Header,Footer,TabContent,$
   },
-  mounted(){
+  setup(){
+    onMounted(()=>{
   $(function(){
   /*-----------------------------------*/
   ///////////////// 變數 ////////////////
@@ -20,68 +22,6 @@ export default {
     wwMedium = 992,
     wwSmall = 768,
     wwxs = 576;
-    /*-----------------------------------*/
-  ////////////////多組Tab////////////////
-  /*-----------------------------------*/
-  var tab_headerHeight = Math.floor($(".header").outerHeight(true));
-  var resizeTimer1;
- 
-  _window.resize(function () {
-    clearTimeout(resizeTimer1);
-    resizeTimer1 = setTimeout(function () {
-      ww = _window.outerWidth();
-      tabSet();
-    }, 50);
-  });
-
-  function tabSet() {
-    $(".tabs").each(function () {
-      var _tab = $(this),
-        _tabItem = _tab.find(".tabItem"),
-        _tabContent = _tab.find(".tabContent"),
-        tabwidth = _tab.width(),
-        tabItemHeight = _tabItem.outerHeight(),
-        tabContentHeight = _tab.find(".active").next().innerHeight(),
-        tiGap = 0,
-        tabItemLength = _tabItem.length,
-        tabItemWidth;
-      _tab.find(".active").next(".tabContent").show();
-      if (ww >= wwSmall) {
-        _tabContent.css("top", tabItemHeight);
-        _tab.height(tabContentHeight + tabItemHeight);
-        tabItemWidth = (tabwidth - (tabItemLength - 1) * tiGap) / tabItemLength;
-      } else {
-        _tab.css("height", "auto");
-        //_tabItem.width(tabwidth);
-        _tabItem.css("margin-left", 0).last().css("position", "relative");
-      }
-      _tabItem.focus(tabs); //改button後，前面改_tabItem
-      _tabItem.click(tabs); //改button後，前面改_tabItem
-      function tabs(e) {
-        var _tabItemNow = $(this), //改button後，原來$(this).parent(),改$(this)
-          tvp = _tab.offset().top,
-          tabIndex = _tabItemNow.index() / 2,
-          scollDistance = tvp + tabItemHeight * tabIndex - tab_headerHeight;
-        _tabItem.removeClass("active");
-        _tabItemNow.addClass("active");
-        if (ww <= wwSmall) {
-          _tabItem.not(".active").next().slideUp();
-          _tabItemNow.next().slideDown();
-          $("html,body")
-            .stop(true, false)
-            .animate({ scrollTop: scollDistance });
-        } else {
-          _tabItem.not(".active").next().hide();
-          _tabItemNow.next().show();
-          tabContentHeight = _tabItemNow.next().innerHeight();
-          _tab.height(tabContentHeight + tabItemHeight);
-        }
-        e.preventDefault();
-      }
-    });
-  }
-  $(".tabs>.tabItem:first-child>a").trigger("click");
-  tabSet();  
   /*-----------------------------------*/
   /////////// 無障礙快捷鍵盤組合  //////////
   /*-----------------------------------*/
@@ -167,6 +107,8 @@ export default {
     _body.find("a.goCenter").focus();
     e.preventDefault();
   });
+  
+})
   }
 }
 </script>
