90d9f86414f8bda485539201a5c1d505175002f3
Your Namemargo-lee <margo.lee@hyweb.com.tw>
2022-02-22 11:56:41 +0800

    更新logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,52 +1,49 @@
 <script>
-export default {
-
-}
+export default {};
 </script>
 <template>
-        <!-- header Start -->
-        <header class="header">
-            <div class="container">
-            <a
-                class="accesskey"
-                href="#aU"
-                id="aU"
-                accesskey="U"
-                title="網站標題"
-                tabindex="2"
-                >:::</a
-            >
-            <!-- navigation Start -->
-            <nav class="navigation" role="navigation" aria-label="Site">
-                <a href="#">
-                    回新竹林管處
-                </a>
-            </nav>    
-            <!-- navigation End -->
-            <div class="">
-                 <div class="h1">
-                      <a href="#                  "
-                    ><img
-                    src="~@/assets/images/img/logo.png"
-                    alt="110年桶后林道車輛進入線上申辦系統建置案"
-                /></a>
-                 </div>
-            </div>
-            <div class="subtitle">
-                <p>
-                通后林道係出入桶后地區唯一道路，<span></span>
-                又區內可供停車空間極為有限，為維持林道暢通確保行車安全、<span></span>
-                保護水源地水質及維護溪流生態景觀等因素考量下，<span></span>
-                林務橘新竹林區管理處理實施「車輛總量管制」措施。
-                </p>
-            </div>
-            </div>
-           
-        </header>
-      <!-- header End -->
+  <!-- header Start -->
+  <header class="header">
+    <div class="container">
+      <a
+        class="accesskey"
+        href="#aU"
+        id="aU"
+        accesskey="U"
+        title="網站標題"
+        tabindex="2"
+        >:::</a
+      >
+      <!-- navigation Start -->
+      <nav class="navigation" role="navigation" aria-label="Site">
+        <a href="#">
+          回新竹林管處
+        </a>
+      </nav>
+      <!-- navigation End -->
+      <div class="">
+        <div class="h1">
+          <a href="#"
+            ><img
+              src="~@/assets/images/img/logo.png"
+              alt="110年桶后林道車輛進入線上申辦系統建置案"
+          /></a>
+        </div>
+      </div>
+      <div class="subtitle">
+        <p>
+          通后林道係出入桶后地區唯一道路，<span
+          ></span> 又區內可供停車空間極為有限，為維持林道暢通確保行車安全、<span
+          ></span> 保護水源地水質及維護溪流生態景觀等因素考量下，<span></span>
+          林務橘新竹林區管理處理實施「車輛總量管制」措施。
+        </p>
+      </div>
+    </div>
+  </header>
+  <!-- header End -->
 </template>
 <style>
-h1{
-    font-size: 0;
+h1 {
+  font-size: 0;
 }
 </style>
